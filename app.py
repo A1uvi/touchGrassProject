68c766ffeb4a2dac54ecbb59974a9c9b7bd62e4c
Alvia Naqvi <alvia.naqvi.08@gmail.com>
2024-10-27 10:19:34 -0400

    Initial Commit II

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ from twilio.rest import Client
 
 app = Flask(__name__)
 
+# new comment
+
 UPLOAD_FOLDER = 'uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
